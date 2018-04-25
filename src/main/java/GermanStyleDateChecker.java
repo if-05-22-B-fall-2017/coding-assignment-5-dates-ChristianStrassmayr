22ee6d0e3fde337dab2256ed1886788ee75508bf
Christian Straßmayr <christian.strassmayr@gmail.com>
2018-04-25 15:41:02 +0200

    finished assignment

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern p = Pattern.compile("^(3[01]|[1-9]|[12][0-9]|0[1-9])[- /.]([1-9]|0[1-9]|1[012])[- /.](19|20)?[0-9][0-9]$");
+        return p.matcher(dateString);
     }
 }
